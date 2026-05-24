183704249f6a281da7ff320ae9ccd8823817b52d
Saloni0404 <you@example.com>
2026-05-23 23:52:37 -0700

    scaling of invite across devices v5

--- src/App.jsx
+++ src/App.jsx
@@ -122,7 +122,7 @@ const toggleMusic = () => {
           <Detail icon={<MapPin/>} label={invite.locationShort} href={invite.mapLink}/>
         </div>
       
-      </div>
+      {/*</div>
       <div className="couple-wrap">
         <img
           src={invite.images.couple}
@@ -130,7 +130,7 @@ const toggleMusic = () => {
           alt="Illustration of Saloni and Shrenik"
         />
       </div>
-    </div>
+    </div>*/}
   </Page>
   <Page n={3}>
     <div className="page3-inner fade-up">
